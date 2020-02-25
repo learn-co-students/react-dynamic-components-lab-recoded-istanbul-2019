6adc2cd991214849f62883335fa035500c84313a
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-02-25 13:14:37 +0300

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,13 @@
 //your code here
+// When we speak we are afraid our words will not be heard or welcomed. But when we are silent, we are still afraid. So it is better to speak. - Audrey Lorde
+import React, { Component } from 'react';
+
+export default class Comments extends Component {
+    render(){
+        return (
+            <div className="comment">
+               {this.props.commentText}
+            </div>
+        )
+    }
+}
