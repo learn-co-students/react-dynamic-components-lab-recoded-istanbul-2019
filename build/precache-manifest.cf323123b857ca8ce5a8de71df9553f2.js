a2bd1801c8e478bda16fa719bba0f86d868cf1be
yusra <github email address>
2020-03-09 02:18:18 +0300

    Done.

---
+++ build/precache-manifest.cf323123b857ca8ce5a8de71df9553f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2112cb4ea525d2ae273e",
+    "url": "/static/js/main.1a21f112.chunk.js"
+  },
+  {
+    "revision": "91d40ea119992fc314f1",
+    "url": "/static/js/2.dc36feb8.chunk.js"
+  },
+  {
+    "revision": "2112cb4ea525d2ae273e",
+    "url": "/static/css/main.ab3ca3ef.chunk.css"
+  },
+  {
+    "revision": "048272386bb4b42fa7c784a9915bd4b2",
+    "url": "/index.html"
+  }
+];
