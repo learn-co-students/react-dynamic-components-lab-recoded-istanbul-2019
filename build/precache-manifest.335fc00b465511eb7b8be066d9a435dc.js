e1c9a08a8a699ec2aabb93f95cc5af08e6fe424a
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-05-27 21:29:22 +0300

    Done.

---
+++ build/precache-manifest.335fc00b465511eb7b8be066d9a435dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47b7555fd042deddf536",
+    "url": "/static/js/main.9bda9234.chunk.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "47b7555fd042deddf536",
+    "url": "/static/css/main.1b15fe2d.chunk.css"
+  },
+  {
+    "revision": "90a519fa5deb41500bb8e58aedf9cd71",
+    "url": "/index.html"
+  }
+];
